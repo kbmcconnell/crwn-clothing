33b27e9834384322e9ec49cc9e1404ea83e2723f
kristonmcconnell <kriston.mcconnell@gmail.com>
2023-08-08 22:33:38 -0700

    updates to checkout and products

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -28,23 +28,33 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
   }
 }
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id)
+
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
-  removeItemFromCart: () => {},
+  removeItemToCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 })
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
   const [cartCount, setCartCount] = useState(0)
+  const [cartTotal, setCartTotal] = useState(0)
 
   useEffect(() => {
     const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
     setCartCount(newCartCount)
+  }, [cartItems])
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+    setCartTotal(newCartTotal)
   }, [cartItems])
 
   const addItemToCart = (productToAdd) => {
@@ -55,13 +65,20 @@ export const CartProvider = ({ children }) => {
     setCartItems(removeCartItem(cartItems, cartItemToRemove))
   }
 
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear))
+  }
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
     removeItemToCart,
+    clearItemFromCart,
     cartItems,
-    cartCount }
+    cartCount,
+    cartTotal,
+  }
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from 'react'
-import PRODUCTS from '../shop-data.json'
+import {createContext, useEffect, useState} from 'react'
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils'
 
 export const ProductsContext = createContext({
   products: [],
@@ -7,7 +7,7 @@ export const ProductsContext = createContext({
 })
 
 export const ProductsProvider = ({children}) => {
-  const [products, setProducts] = useState(PRODUCTS)
+  const [products, setProducts] = useState([])
   const value = {products}
   return (
     <ProductsContext.Provider value={value}>{ children }</ProductsContext.Provider>
